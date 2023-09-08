8fe74621c4d4707b493743c65d9fb0c006603292
Fransnico Susanto <fransnico@amazeworx.com>
2023-09-08 07:58:37 +0700

    breadcrumb

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -6,16 +6,19 @@ function nswnma_yoast_seo_breadcrumb_append_link($links)
   global $post;
 
   if (is_singular('post')) {
-    $breadcrumb[] = array(
-      'url' => '',
-      'text' => 'Latest News',
-    );
-    array_splice($links, 1, -2, $breadcrumb);
-  } elseif (is_tax('industry')) {
-    $breadcrumb[] = array(
-      'url' => site_url('/industry/'),
-      'text' => 'Industry',
-    );
+    $categories = get_the_category();
+    //preint_r($categories);
+    if ($categories[0]->slug == 'jobs-at-nswnma') {
+      $breadcrumb[] = array(
+        'url' => '',
+        'text' => $categories[0]->name,
+      );
+    } else {
+      $breadcrumb[] = array(
+        'url' => '',
+        'text' => 'Latest News',
+      );
+    }
     array_splice($links, 1, -2, $breadcrumb);
   }
 
