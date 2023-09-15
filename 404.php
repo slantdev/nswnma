648a798bfdc3879e6449a9af5057b285e3fa4e61
Fransnico Susanto <fransnico@amazeworx.com>
2023-09-15 08:31:01 +0700

    404

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
         <div class="text-5xl md:text-15xl text-gray-800 border-brand-blue border-b">404</div>
         <div class="w-16 h-1 my-3 md:my-4"></div>
         <p class="text-gray-800 text-2xl md:text-3xl font-light mb-8"><?php _e('Sorry, the page you are looking for could not be found.', 'nswnma'); ?></p>
-        <a href="<?php echo get_bloginfo('url'); ?>" class="bg-brand-blue px-4 py-2 rounded text-white">
+        <a href="<?php echo get_bloginfo('url'); ?>" class="btn btn-primary">
           <?php _e('Go to Homepage', 'nswnma'); ?>
         </a>
       </div>
