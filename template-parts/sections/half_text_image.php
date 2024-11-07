5424a0e7df2b8928ad8197133609e1fb833cea24
Slant Dev <build@amazeworx.com>
2024-11-07 14:17:49 +0700

    fix warnings

--- template-parts/sections/half_text_image.php
+++ template-parts/sections/half_text_image.php
@@ -9,30 +9,37 @@ include get_template_directory() . '/template-parts/layouts/section_settings.php
 */
 $column_image = get_sub_field('column_image');
 $column_text = get_sub_field('column_text');
+$column_text_title = $column_text['title'] ?? '';
+$column_image_url = $column_image['image']['url'] ?? '';
+$column_text_text = $column_text['text'] ?? '';
+$column_text_button_link = $column_text['button_link'] ?? '';
+$column_text_button_link_url = $column_text['button_link']['url'] ?? '';
+$column_text_button_link_title = $column_text['button_link']['title'] ?? '';
 
-if ($column_text['title']) : ?>
+
+if ($column_text_title) : ?>
   <section id="<?php echo $section_id ?>" style="<?php echo $section_style ?>">
     <div class="relative <?php echo $section_padding_top . ' ' . $section_padding_bottom ?>">
 
       <div class="flex flex-col lg:hidden">
         <div class="w-full">
-          <?php if ($column_image['image']) : ?>
+          <?php if ($column_image_url) : ?>
             <div class="aspect-w-16 aspect-h-9">
-              <img src="<?php echo $column_image['image']['url'] ?>" alt="" class="object-cover h-full w-full">
+              <img src="<?php echo $column_image_url ?>" alt="" class="object-cover h-full w-full">
             </div>
           <?php endif; ?>
           <div class="p-6 pb-8">
-            <?php if ($column_text['title']) : ?>
-              <h2 class="h3 text-brand-blue mb-4 mt-4"><?php echo $column_text['title'] ?></h2>
+            <?php if ($column_text_title) : ?>
+              <h2 class="h3 text-brand-blue mb-4 mt-4"><?php echo $column_text_title ?></h2>
             <?php endif; ?>
-            <?php if ($column_text['text']) : ?>
+            <?php if ($column_text_text) : ?>
               <div class="prose">
-                <p><?php echo $column_text['text'] ?></p>
+                <p><?php echo $column_text_text ?></p>
               </div>
             <?php endif; ?>
-            <?php if ($column_text['button_link']) : ?>
+            <?php if ($column_text_button_link_url) : ?>
               <div class="mt-4">
-                <a href="<?php echo $column_text['button_link']['url'] ?>" class="btn btn-primary"><?php echo $column_text['button_link']['title'] ?></a>
+                <a href="<?php echo $column_text_button_link_url ?>" class="btn btn-primary"><?php echo $column_text_button_link_title ?></a>
               </div>
             <?php endif; ?>
           </div>
@@ -41,7 +48,7 @@ if ($column_text['title']) : ?>
 
       <div class="container hidden lg:block">
         <?php
-        $image_position = $column_image['image_position'];
+        $image_position = $column_image['image_position'] ?? '';
         if ($image_position == 'left') :
         ?>
           <div class="flex flex-col lg:flex-row lg:justify-end">
@@ -53,17 +60,17 @@ if ($column_text['title']) : ?>
                     <div class="py-8 lg:pr-16 lg:py-20">
                     <?php endif; ?>
 
-                    <?php if ($column_text['title']) : ?>
-                      <h2 class="h3 text-brand-blue mb-8 mt-8"><?php echo $column_text['title'] ?></h2>
+                    <?php if ($column_text_title) : ?>
+                      <h2 class="h3 text-brand-blue mb-8 mt-8"><?php echo $column_text_title ?></h2>
                     <?php endif; ?>
-                    <?php if ($column_text['text']) : ?>
+                    <?php if ($column_text_text) : ?>
                       <div class="prose">
-                        <p><?php echo $column_text['text'] ?></p>
+                        <p><?php echo $column_text_text ?></p>
                       </div>
                     <?php endif; ?>
-                    <?php if ($column_text['button_link']) : ?>
+                    <?php if ($column_text_button_link_url) : ?>
                       <div class="mt-8">
-                        <a href="<?php echo $column_text['button_link']['url'] ?>" class="btn btn-primary"><?php echo $column_text['button_link']['title'] ?></a>
+                        <a href="<?php echo $column_text_button_link_url ?>" class="btn btn-primary"><?php echo $column_text_button_link_title ?></a>
                       </div>
                     <?php endif; ?>
                     </div>
@@ -72,7 +79,7 @@ if ($column_text['title']) : ?>
               </div>
               <?php if ($column_image['image']) : ?>
                 <?php
-                $image_position = $column_image['image_position'];
+                $image_position = $column_image['image_position'] ?? '';
                 $col_image_style = '';
                 if ($image_position == 'left') {
                   $col_image_style = 'left: 0; right: 50%;';
@@ -81,7 +88,7 @@ if ($column_text['title']) : ?>
                 }
                 ?>
                 <div class="hidden lg:block absolute top-0 right-0 bottom-0 left-1/2 h-full" style="<?php echo $col_image_style ?>">
-                  <img src="<?php echo $column_image['image']['url'] ?>" alt="" class="object-cover h-full w-full">
+                  <img src="<?php echo $column_image_url ?>" alt="" class="object-cover h-full w-full">
                 </div>
               <?php endif; ?>
             </div>
