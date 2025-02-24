c78208964e1319a44fac4f6631b7dae77f7e0a97
Slant Dev <build@amazeworx.com>
2025-02-24 09:52:33 +0700

    fix download guides error

--- inc/utilities.php
+++ inc/utilities.php
@@ -11,3 +11,32 @@ function nswnma_video_container($html)
 }
 add_filter('embed_oembed_html', 'nswnma_video_container', 10, 3);
 add_filter('video_embed_html', 'nswnma_video_container'); // Jetpack
+
+
+function card_guides($atts = array())
+{
+  $atts = shortcode_atts(array(
+    'image' => nswnma_asset('images/guides/virus.png'),
+    'title'  => '',
+    'download_url'  => '#',
+    'bg_color'  => '#7DBBC8',
+  ), $atts);
+  $output = '';
+  $output .= '<div>
+    <div class="aspect-w-1 aspect-h-1">
+      <div class="rounded-lg flex p-6 items-center justify-center shadow-lg" style="background-color: ' . $atts['bg_color'] . ';">
+        <img src="' . $atts['image'] . '" alt="">
+      </div>
+    </div>
+    <div class="pt-4 pb-4">
+      <h4 class="text-xl font-medium text-brand-bluedark line-clamp-2 h-14">' . $atts['title'] . '</h4>
+    </div>
+    <div class="border-t flex gap-x-2 pt-[8px] hover:border-brand-bluedark hover:border-t-4 hover:pt-[5px]">
+      <a href="' . $atts['download_url'] . '" target="_blank" class="inline-block text-gray-500 hover:text-brand-bluedark">
+        ' . nswnma_icon(array('icon' => 'download', 'group' => 'utilities', 'size' => '32', 'class' => '')) . '
+      </a>
+    </div>
+  </div>';
+
+  return $output;
+};
--- template-parts/sections/download_guides.php
+++ template-parts/sections/download_guides.php
@@ -11,34 +11,6 @@ $section_intro = get_sub_field('section_intro');
 $headline = $section_intro['headline'] ?? '';
 $description = $section_intro['description'] ?? '';
 $guides_grid = get_sub_field('guides_grid');
-
-function card_guides($atts = array())
-{
-  $atts = shortcode_atts(array(
-    'image' => nswnma_asset('images/guides/virus.png'),
-    'title'  => '',
-    'download_url'  => '#',
-    'bg_color'  => '#7DBBC8',
-  ), $atts);
-  $output = '';
-  $output .= '<div>
-    <div class="aspect-w-1 aspect-h-1">
-      <div class="rounded-lg flex p-6 items-center justify-center shadow-lg" style="background-color: ' . $atts['bg_color'] . ';">
-        <img src="' . $atts['image'] . '" alt="">
-      </div>
-    </div>
-    <div class="pt-4 pb-4">
-      <h4 class="text-xl font-medium text-brand-bluedark line-clamp-2 h-14">' . $atts['title'] . '</h4>
-    </div>
-    <div class="border-t flex gap-x-2 pt-[8px] hover:border-brand-bluedark hover:border-t-4 hover:pt-[5px]">
-      <a href="' . $atts['download_url'] . '" target="_blank" class="inline-block text-gray-500 hover:text-brand-bluedark">
-        ' . nswnma_icon(array('icon' => 'download', 'group' => 'utilities', 'size' => '32', 'class' => '')) . '
-      </a>
-    </div>
-  </div>';
-
-  return $output;
-};
 ?>
 <section id="<?php echo $section_id ?>" style="<?php echo $section_style ?>">
   <div class="relative <?php echo $section_padding_top . ' ' . $section_padding_bottom ?>">
